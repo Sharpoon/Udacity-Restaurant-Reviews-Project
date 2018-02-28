0d808525b0094542f1eaacac771233610704dce7
Steve Harper <steve@harpnet.co.uk>
2018-02-28 15:39:44 +0000

    Fix layout responsive issues and update color scheme to be accesibility friendly

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -117,13 +117,15 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
  */
 createReviewHTML = (review) => {
   const li = document.createElement('li');
+  const header = document.createElement('header');
+  li.appendChild(header)
   const name = document.createElement('p');
   name.innerHTML = review.name;
-  li.appendChild(name);
+  header.appendChild(name);
 
   const date = document.createElement('p');
   date.innerHTML = review.date;
-  li.appendChild(date);
+  header.appendChild(date);
 
   const rating = document.createElement('p');
   rating.innerHTML = `Rating: ${review.rating}`;
@@ -134,7 +136,7 @@ createReviewHTML = (review) => {
   li.appendChild(comments);
 
   return li;
-}
+};
 
 /**
  * Add restaurant name to the breadcrumb navigation menu
